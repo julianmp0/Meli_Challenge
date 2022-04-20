1247164ff9817b8a361a831b44c2c053413e049d
Julian Martinez <julianmp0@gmail.com>
2022-04-20 11:59:51 -0500

    add: implementation of Dagger Hilt

--- app/src/main/java/com/meli/challenge/MainActivity.kt
+++ app/src/main/java/com/meli/challenge/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.colorResource
